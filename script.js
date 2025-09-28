3408e3e001d3b341cec25f3ddc5ef79af37b93c6
Mikal Paudel <mikalpaudel58@gmail.com>
2025-09-28 18:30:18 +0545

    Add validation functions for username and email input fields

---
+++ script.js
@@ -0,0 +1,36 @@
+const nameErr = document.getElementById('name-error');
+const emailErr = document.getElementById('email-error');
+const phoneErr = document.getElementById('phone-error');
+const messageErr = document.getElementById('msg-error');
+const username = document.getElementById('username');
+
+function validateName(){
+    if(username.value.length == 0){
+        nameErr.innerHTML = 'Name is required';
+        nameErr.style.display = 'block';
+        return false;
+    }
+    if (!username.value.match(/^[A-Za-z]+ [A-Za-z]+$/)){
+        nameErr.innerHTML = 'Enter Full Name';
+        return false;
+    }
+    nameErr.innerHTML = 'valid'
+    return true;
+}
+
+function validateEmail() {
+    const email = document.getElementById('email').value;
+
+    if(email.length == 0){
+        emailErr.style.display = 'block'
+        emailErr.innerHTML = 'Email is required';
+        return false;
+    }
+    if(!email.match(/^[a-z0-9!#$%&'\*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'\*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/)){
+        emailErr.innerHTML = 'Enter valid email';
+        return false;
+        }
+    emailErr.innerHTML = 'valid';
+    return true;
+}
+
